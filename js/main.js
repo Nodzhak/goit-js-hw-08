b81415311fdeb6a7ccf138d3af57c2263097e1cf
Nodzhak <kostiantyn.nodzhak@gmail.com>
2023-11-20 10:49:13 +0100

    End
    Правки до пункту 9-го завдання.

--- js/main.js
+++ js/main.js
@@ -1,94 +1,93 @@
 const images = [
-  {
-    preview:
+  { preview:
       'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg',
     original:
       'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
-    description: 'Hokkaido Flower',
-  },
-  {
-    preview:
+    description: 'Hokkaido Flower',},
+  { preview:
       'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
     original:
       'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
-    description: 'Container Haulage Freight',
-  },
-  {
-    preview:
+    description: 'Container Haulage Freight',},
+  { preview:
       'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
     original:
       'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
-    description: 'Aerial Beach View',
-  },
-  {
-    preview:
+    description: 'Aerial Beach View',},
+  { preview:
       'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
     original:
       'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
-    description: 'Flower Blooms',
-  },
-  {
-    preview:
+    description: 'Flower Blooms',},
+  { preview:
       'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
     original:
       'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
-    description: 'Alpine Mountains',
-  },
-  {
-    preview:
+    description: 'Alpine Mountains',},
+  { preview:
       'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
     original:
       'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
-    description: 'Mountain Lake Sailing',
-  },
-  {
-    preview:
+    description: 'Mountain Lake Sailing',},
+  { preview:
       'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
     original:
       'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
-    description: 'Alpine Spring Meadows',
-  },
-  {
-    preview:
+    description: 'Alpine Spring Meadows',},
+  { preview:
       'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
     original:
       'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
-    description: 'Nature Landscape',
-  },
-  {
-    preview:
+    description: 'Nature Landscape',},
+  { preview:
       'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
     original:
       'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
-    description: 'Lighthouse Coast Sea',
-  },
-];
+    description: 'Lighthouse Coast Sea',},
+]
+
+      // Контейнер галереї
 
 const galleryContainer = document.querySelector('.gallery');
 
-            // Делегування
+      // Модальне вікно
 
+let instance = null;
+
+      // Відкриваємо модальне вікно
+   
 galleryContainer.addEventListener('click', function (event) {
   event.preventDefault();
+
+      // Чи є клік на зображення?
 
   if (event.target.classList.contains('gallery-image')) {
     const largeImage = event.target.dataset.source;
 
-            // Модальне вікно
+      // Закриття попереднього модального вікна
 
-    const instance = basicLightbox.create(`<img src="${largeImage}" alt="Large Image">`,);
+    if (instance) {instance.close();}
+
+      // Нове модальне вікно
+
+    instance = basicLightbox.create(`<img src="${largeImage}" alt="Large Image">`,);
     instance.show();
+     
+      // Чи натиснуто "esc"?
 
-    document.addEventListener('keydown', function (event) {
-      if (event.key === 'Escape') {
-        instance.close();
-      }
-    })
-  }
-});
+    document.addEventListener('keydown', closeOnEscape);}})
 
-            // Розмітка елементів галереї
+      // Закриття модального вікан при кліку на "esc"
 
+function closeOnEscape(event) {
+  if (event.key === 'Escape' && instance) {
+    instance.close();
+
+      // Видалення прослуховування події після закриття вікна.
+
+    document.removeEventListener('keydown', closeOnEscape);}}
+
+// Розмітка елементів галереї
 images.forEach((image) => {
   const list = document.createElement('li');
   list.classList.add('gallery-item');
@@ -97,8 +96,7 @@ images.forEach((image) => {
   a.href = image.original;
   a.classList.add('gallery-link');
   a.addEventListener('click', function (event) {
-    event.preventDefault();
-  });
+    event.preventDefault();})
 
   const img = document.createElement('img');
   img.src = image.preview;
@@ -109,5 +107,4 @@ images.forEach((image) => {
   a.appendChild(img);
   list.appendChild(a);
 
-  galleryContainer.appendChild(list);
-});
+  galleryContainer.appendChild(list);})
